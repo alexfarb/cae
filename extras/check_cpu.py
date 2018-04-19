083d93331fad6e4c48fa0a69561330c28e433273
Alexandre Farias <alexfarb@gmail.com>
2018-04-19 19:46:47 -0300

    ECG e Arritmias
    Adicionando o material sobre ECG e Arritmias.

---
+++ extras/check_cpu.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 10 18:01:14 2018
+
+@author: Avell
+"""
+
+import tensorflow as tf
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+
+with tf.Session() as sess:
+    print (sess.run(c))
