67791fcb26d86227a39a51b033700261788ace9d
Alexandre Farias <alexfarb@gmail.com>
2018-04-23 23:08:51 -0300

    Organização das Toolboxes
    wfdb adicionado

--- extras/check_modules.py
+++ extras/check_modules.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 10 17:59:40 2018
+import pkgutil
 
-@author: Avell
-"""
+x = pkgutil.iter_modules()
 
-import pip
-installed_packages = pip.get_installed_distributions()
-installed_packages_list = sorted(["%s==%s" % (i.key, i.version)
-     for i in installed_packages])
-print(installed_packages_list)
+print(x)
---
+++ qrs_detector.py
@@ -0,0 +1,3 @@
+from QRSDetectorOffline import QRSDetectorOffline
+
+qrs_detector = QRSDetectorOffline(ecg_data_path="C://repos//cae//data//diag//standard//all//100m.csv", verbose=True, log_data=True, plot_data=True, show_plot=True)
