2b42778a8be67d926aaeee82d1b351ab359fad21
Alexandre Farias <alexfarb@gmail.com>
2018-04-28 17:15:52 -0300

    Treinamento Funcionando
    Treinamento Funcionando para o CAE

--- data/diag/standard/healthy/loadall.m
+++ data/diag/standard/healthy/loadall.m
@@ -8,6 +8,6 @@ all_vectors = [X100m;X101m;X103m;X105m;X106m;X111m;
                     X117m;X118m;X121m;X122m;X123m;X124m;
                     X205m;X215m;X220m;X223m;X230m;X234m;];
     
-healthy_samples_std = all_vectors';
+healthy_samples_std = all_vectors;
 
-csvwrite('C:\repos\cae\data\new\healthy_samples_std.csv',healthy_samples_std);
+csvwrite('C:\repos\cae\data\conv1d\healthy_samples_std.csv',healthy_samples_std);
