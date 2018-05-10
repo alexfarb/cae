60d2641026f1c1e09b8475dd630be0d935672f59
Alexandre Farias <alexfarb@gmail.com>
2018-05-10 15:53:52 -0300

    Redes pra Treinamento das bases de dados normal e anormal finalizadas

--- cae.py
+++ cae.py
@@ -81,8 +81,10 @@ class Autoencoder(object):
         autoencoder.compile(optimizer=optimizer_option, loss=loss_option)
         
         # Treinamento do Modelo
-        autoencoder.fit(x_train,y_train,
+        history_callback = autoencoder.fit(x_train,y_train,
                          epochs=epochs_num)
+        
+        loss_history = history_callback.history["loss"]
         
         # Saída do Treinamento
         decoded_train = autoencoder.predict(x_train)
@@ -90,7 +92,7 @@ class Autoencoder(object):
         decoded_train_reshaped = (decoded_train.reshape(self.data_train_num, self.data_length))
         # Erro de Treinamento
         train_error = self.input_train - decoded_train_reshaped
-        return decoded_train_reshaped, train_error
+        return decoded_train_reshaped, train_error, loss_history
 
 # Classe para os Plots
 class Plots(object):
@@ -115,32 +117,66 @@ class Plots(object):
         plt.xlim(0,data_length)
         plt.show()
 
-# Pré-processamento e parâmetros para a Base de Dados Saudável (Rede A)
-dataset_a = np.loadtxt("C:\\repos\\cae\\data\\conv1d\\healthy_samples_std.csv",delimiter=",") # Carrega a Base de Dados
-data_length_a = len(dataset_a.T) # Tamanho do sinal em número de amostras
-data_train_num_a = 14 # Tamanho do conjunto de treinamento
-data_test_num_a = 18 # Última amostra do conjunto de teste
-kernel_size_a =  20 # Tamanho do Kernel (Janela) de Convolução
-epochs_num_a = 800 # Quantidade de Épocas para o Treinamento da Rede
-data_dimension_a = 1 # Dimensão dos Dados
-optimizer_a = 'adamax'
-loss_a = 'mean_squared_error'
-# Divisão em treino e teste
-pre_processing_a = PreProcessingData(dataset_a)
-input_train_a, output_train_a, input_test_a, output_test_a = pre_processing_a.split_train_test(data_train_num_a, data_test_num_a, data_length_a)
-# Minimos e máximos do Conjunto de treino
-max_a = input_train_a.max()
-min_a = input_train_a.min()
-# Normalização das entradas e saídas
-x_train_a = pre_processing_a.normalize_data(input_train_a,min_a,max_a)
-y_train_a = x_train_a
-x_test_a = pre_processing_a.normalize_data(input_test_a,min_a,max_a)
-y_test_a = x_test_a
+network_num = 2
 
-# Autoencoder para Rede A
-auto_encoder_a = Autoencoder(x_train_a, x_test_a, y_train_a, y_test_a, data_length_a, data_train_num_a, data_test_num_a, data_dimension_a)
-decoded_train_a, train_error_a = auto_encoder_a.convolutional_autoencoder_1d(kernel_size_a, epochs_num_a, optimizer_a, loss_a)
+if network_num == 1:
+    # Pré-processamento e parâmetros para a Base de Dados Saudável (Rede A)
+    dataset_a = np.loadtxt("C:\\repos\\cae\\data\\conv1d\\healthy_samples_std.csv",delimiter=",") # Carrega a Base de Dados
+    data_length_a = len(dataset_a.T) # Tamanho do sinal em número de amostras
+    data_train_num_a = 14 # Tamanho do conjunto de treinamento
+    data_test_num_a = 18 # Última amostra do conjunto de teste
+    kernel_size_a =  20 # Tamanho do Kernel (Janela) de Convolução
+    epochs_num_a = 2 # Quantidade de Épocas para o Treinamento da Rede
+    data_dimension_a = 1 # Dimensão dos Dados
+    optimizer_a = 'adamax'
+    loss_a = 'mean_squared_error'
+    # Divisão em treino e teste
+    pre_processing_a = PreProcessingData(dataset_a)
+    input_train_a, output_train_a, input_test_a, output_test_a = pre_processing_a.split_train_test(data_train_num_a, data_test_num_a, data_length_a)
+    # Minimos e máximos do Conjunto de treino
+    max_a = input_train_a.max()
+    min_a = input_train_a.min()
+    # Normalização das entradas e saídas
+    x_train_a = pre_processing_a.normalize_data(input_train_a,min_a,max_a)
+    y_train_a = x_train_a
+    x_test_a = pre_processing_a.normalize_data(input_test_a,min_a,max_a)
+    y_test_a = x_test_a
 
-# Plots para a Rede A
-plot = Plots(x_train_a, decoded_train_a)
-plot.plot_reconstruct(100, train_error_a, 0, data_length_a)
+    # Autoencoder para Rede A
+    auto_encoder_a = Autoencoder(x_train_a, x_test_a, y_train_a, y_test_a, data_length_a, data_train_num_a, data_test_num_a, data_dimension_a)
+    decoded_train_a, train_error_a, loss_a = auto_encoder_a.convolutional_autoencoder_1d(kernel_size_a, epochs_num_a, optimizer_a, loss_a)
+
+    # # Plots para a Rede A
+    # plot = Plots(x_train_a, decoded_train_a)
+    # plot.plot_reconstruct(100, train_error_a, 0, data_length_a)
+
+else:
+    # Pré-processamento e parâmetros para a Base de Dados com anormalidade (Rede B)
+    dataset_b = np.loadtxt("C:\\repos\\cae\\data\\conv1d\\abnormal_samples_std.csv",delimiter=",") # Carrega a Base de Dados
+    data_length_b = len(dataset_b.T) # Tamanho do sinal em número de amostras
+    data_train_num_b = 20 # Tamanho do conjunto de treinamento
+    data_test_num_b = 28 # Última amostra do conjunto de teste
+    kernel_size_b =  20 # Tamanho do Kernel (Janela) de Convolução
+    epochs_num_b = 10000 # Quantidade de Épocas para o Treinamento da Rede
+    data_dimension_b = 1 # Dimensão dos Dados
+    optimizer_b = 'adamax'
+    loss_b = 'mean_squared_error'
+    # Divisão em treino e teste
+    pre_processing_b = PreProcessingData(dataset_b)
+    input_train_b, output_train_b, input_test_b, output_test_b = pre_processing_b.split_train_test(data_train_num_b, data_test_num_b, data_length_b)
+    # Minimos e máximos do Conjunto de treino
+    max_b = input_train_b.max()
+    min_b = input_train_b.min()
+    # Normalização das entradas e saídas
+    x_train_b = pre_processing_b.normalize_data(input_train_b,min_b,max_b)
+    y_train_b = x_train_b
+    x_test_b = pre_processing_b.normalize_data(input_test_b,min_b,max_b)
+    y_test_b = x_test_b
+
+    # Autoencoder para Rede B
+    auto_encoder_b = Autoencoder(x_train_b, x_test_b, y_train_b, y_test_b, data_length_b, data_train_num_b, data_test_num_b, data_dimension_b)
+    decoded_train_b, train_error_b, loss_b = auto_encoder_b.convolutional_autoencoder_1d(kernel_size_b, epochs_num_b, optimizer_b, loss_b)
+
+    # # Plots para a Rede B
+    # plot = Plots(x_train_b, decoded_train_b)
+    # plot.plot_reconstruct(107, train_error_b, 0, data_length_b)
