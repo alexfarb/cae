4b6d5abce1cb185c8b4a75fb29b2765f91941c29
Alexandre Farias <alexfarb@gmail.com>
2018-05-28 19:31:00 -0300

    Teste com o autoencoder padrão.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import cae
 import cae_val
+import autoenc
+import autoenc_val
 
 # cae.main_cae()
-cae_val.main_cae_val()
+# cae_val.main_cae_val()
+# autoenc.main_autoenc()
+autoenc_val.main_autoenc_val()
